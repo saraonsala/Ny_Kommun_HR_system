024aa33d7bf47e4064c2760212f8f75185b67c46
Sara Mellgren <sarasbetongochtradgard@gmail.com>
2024-01-23 12:20:26 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = "server=localhost;user=root;database=en_kommuns_hr_system";
+
+        using (IDbConnection db = new MySqlConnection(connectionString))
+        {
+            var result = db.Query("SELECT * FROM Employees");
+            foreach (var row in result)
+            {
+                Console.WriteLine(row.Name);
+
+            }
+        }
+    }
+}
