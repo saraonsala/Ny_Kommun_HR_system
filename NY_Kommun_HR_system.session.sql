fb70659e02a82b9450bb5674dfffc137c6773a83
Sara Mellgren <sarasbetongochtradgard@gmail.com>
2024-01-29 11:31:16 +0100

    Employees, test

--- NY_Kommun_HR_system.session.sql
+++ NY_Kommun_HR_system.session.sql
@@ -1,2 +1,6 @@
-SELECT * 
-FROM empl
+-- Active: 1706018111580@@127.0.0.1@3306
+SELECT *
+
+FROM employees;
+
+
--- UI.cs
+++ UI.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
 using System.Data;
 using MySql.Data.MySqlClient;
 using Dapper;
 
-class Program
+class UI
 {
     static void Main(string[] args)
     {
-        //string connectionString = "server=localhost;user=root;database=en_kommuns_hr_system";
+        
 
-        //using (IDbConnection db = new MySqlConnection(connectionString))
+   
         {
             var result = db.Query("SELECT * FROM Employees");
             foreach (var row in result)
